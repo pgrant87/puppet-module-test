6e9020c574ea496097fb7c47afba52a978dea202
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2022-08-24 15:26:59 +0100

    Create snapshots.ps1

---
+++ tasks/snapshots.ps1
@@ -0,0 +1,28 @@
+#!/usr/bin/pwsh
+$password    = $args[0]
+$dateMonth   = Get-Date -Format "MMMM"
+$serverArray = $args[1]
+
+Connect-VIServer -Server "vspherehost" -Protocol https -User "vsphereuser" -Password $password
+
+foreach ($server in $serverArray){
+  $powerstate = (Get-VM -Name "$server" | Select-Object PowerState).PowerState
+  Shutdown-VMGuest -VM $server -Confirm:$False
+  if($?){
+       echo "$server gracefully shutdown started"
+    }
+  else{
+       Stop-VM -VM $server -Confirm:$False
+       echo "$server poweroff started"
+
+    }
+  while ($powerstate -ne "PoweredOff") {
+    echo $powerstate
+    Start-Sleep -Seconds 3
+    $powerstate = (Get-VM -Name "$server" | Select-Object PowerState).PowerState
+  }
+  if(New-Snapshot -VM $server -Name "before update $dateMonth" -Description "Testing this out" -Quiesce -Memory){
+    echo "Snapshot created for $server"
+    Start-VM -VM $server -Confirm:$False
+  }
+}
