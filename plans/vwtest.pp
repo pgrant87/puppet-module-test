0b34e3523c4252008d30f45937234084e798fa84
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2022-09-05 15:35:27 +0100

    Update vwtest.pp

--- plans/vwtest.pp
+++ plans/vwtest.pp
@@ -1,4 +1,7 @@
-plan vwtest(TargetSpec $targets) {
-  # This collects facts on targets and updates the inventory
-  run_task('facter_task', 'targets' => $targets)
+plan hello_world () {
+  out::message('hello world')
 }
+#plan vwtest(TargetSpec $targets) {
+  # This collects facts on targets and updates the inventory
+#  run_task('facter_task', 'targets' => $targets)
+#}
