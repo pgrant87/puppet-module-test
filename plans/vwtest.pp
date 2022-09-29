d615c405f0d6cddc47f8093ca64fa41586fbd480
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2022-09-29 13:26:35 +0100

    Update vwtest.pp

--- plans/vwtest.pp
+++ plans/vwtest.pp
@@ -1,5 +1,6 @@
 plan redwoodtest::vwtest ($targets) {
+  $outside_apply = lookup('test_key')
   #$test = ${test_key}
-  return "${test_key}"
+  return "$outside_apply"
   #return run_task('facter_task', $targets)
 }
