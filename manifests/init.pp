8ec519a70f0451f791fb310ff0e817ec067f336e
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2022-10-17 17:55:10 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,5 @@ class redwoodtest {
 
 notify{"The value is: ${envtestvar}": }
 
-$myhash = {'test' => 'val'}
-
-file { '/tmp/config.toml':
-  ensure  => file,
-  content => to_toml($myhash),
-}
 
 }
