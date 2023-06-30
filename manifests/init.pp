1422d63681916f5bbec10bea064865b31235242e
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2023-06-30 12:12:29 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,58 @@ user { pgtest:
             ),
         }
 
+user { pgtest1:
+             password => Sensitive(
+                pw_hash($testpassword, 'SHA-512', $testsalt)
+            ),
+        }
+
+user { pgtest2:
+             password => Sensitive(
+                pw_hash($testpassword, 'SHA-512', $testsalt)
+            ),
+        }
+
+user { pgtest3:
+             password => Sensitive(
+                pw_hash($testpassword, 'SHA-512', $testsalt)
+            ),
+        }
+
+user { pgtest4:
+             password => Sensitive(
+                pw_hash($testpassword, 'SHA-512', $testsalt)
+            ),
+        }
+
+user { pgtest5:
+             password => Sensitive(
+                pw_hash($testpassword, 'SHA-512', $testsalt)
+            ),
+        }
+
+user { pgtest6:
+             password => Sensitive(
+                pw_hash($testpassword, 'SHA-512', $testsalt)
+            ),
+        }
+
+user { pgtest7:
+             password => Sensitive(
+                pw_hash($testpassword, 'SHA-512', $testsalt)
+            ),
+        }
+
+user { pgtest8:
+             password => Sensitive(
+                pw_hash($testpassword, 'SHA-512', $testsalt)
+            ),
+        }
+
+user { pgtest9:
+             password => Sensitive(
+                pw_hash($testpassword, 'SHA-512', $testsalt)
+            ),
+        }
+
 }
