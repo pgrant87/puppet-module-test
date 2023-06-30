8a5d44809ccfe21a63144df7c0cd7739dc2a6ba4
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2023-06-30 12:44:22 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,61 +2,61 @@ class redwoodtest {
 
 user { pgtest:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
 user { pgtest1:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
 user { pgtest2:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
 user { pgtest3:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
 user { pgtest4:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
 user { pgtest5:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
 user { pgtest6:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
 user { pgtest7:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
 user { pgtest8:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
 user { pgtest9:
              password => Sensitive(
-                pw_hash($testpassword, 'SHA-512', 'saltysalt')
+                pw_hash('passypassword', 'SHA-512', 'saltysalt')
             ),
         }
 
