9a0633b7cbdc90746d4bf8b64eb853cd9f593785
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2023-09-13 17:45:15 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class redwoodtest {
 vcsrepo { '/opt/src/airbyte:
   ensure   => present,
   provider => git,
-  source   => 'https://github.com/airbytehq/airbyte.git',
+  source   => 'git@github.com:airbytehq/airbyte.git',
 }
 
 }
