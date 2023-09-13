18244f2813cb9c02cd1664e0a57f7ecaa79992ec
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2023-09-13 17:21:29 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,63 +1,9 @@
 class redwoodtest {
 
-user { pgtest:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
-
-user { pgtest1:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
-
-user { pgtest2:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
-
-user { pgtest3:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
-
-user { pgtest4:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
-
-user { pgtest5:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
-
-user { pgtest6:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
-
-user { pgtest7:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
-
-user { pgtest8:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
-
-user { pgtest9:
-             password => Sensitive(
-                pw_hash('passypassword', 'SHA-512', 'saltysalt')
-            ),
-        }
+vcsrepo { '/root/airbyte':
+  ensure   => present,
+  provider => git,
+  source   => 'git@github.com:airbytehq/airbyte.git',
+}
 
 }
