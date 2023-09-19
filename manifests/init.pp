2e3c8283979ca958164c25d169dfa25a568bffc6
Patrick Grant <32844876+pgrant87@users.noreply.github.com>
2023-09-19 13:12:26 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,34 @@
-class redwoodtest {
+class test {
 
-vcsrepo { '/opt/src/airbyte:
-  ensure   => present,
-  provider => git,
-  source   => 'https://github.com/airbytehq/airbyte.git',
-}
+    # You can use this class to deploy Airbyte on a server.
+
+    # This manifest will:      
+    # 1. Install Docker
+    # 2. Install Docker Compose
+    # 3. Install Git
+    # 4. Clone the Airbyte repository
+    # 5. Run the Airbyte installation script
+
+    include docker, docker::compose, git
+
+    file { '/tmp/airbyte':
+    ensure => directory,
+    }
+
+    vcsrepo { 'airbyte repo':
+    path     => '/tmp/airbyte',
+    ensure   => present,
+    provider => git,
+    source   => "https://github.com/airbytehq/airbyte.git",
+    require  => File['/tmp/airbyte'],
+    }
+
+    exec {runinstallscript:
+    command => 'bash run-ab-platform.sh',
+    cwd     => '/tmp/airbyte',
+    require => Vcsrepo['airbyte repo'],
+    timeout => 0,
+    }
+
 
 }
